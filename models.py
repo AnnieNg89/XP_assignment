9b91ab32b1ddc6782e1d30602762ed5996b7cfcd
An Nguyen <114642252+AnnieNg89@users.noreply.github.com>
2024-05-19 22:34:25 -0700

    Delete models.py

--- models.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class Task(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(80),nullable=False)
-    completed = db.Column(db.Boolean,default=False)
